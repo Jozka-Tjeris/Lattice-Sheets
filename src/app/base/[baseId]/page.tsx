6e8040a7874f3f5fa102ab5dac66f1b2f371887e
Jozka Tjeris <jozkant@gmail.com>
2026-01-30 15:21:16 +0700

    update: changed favicon, app title and dynamically generated website title based on base name

--- src/app/base/[baseId]/page.tsx
+++ src/app/base/[baseId]/page.tsx
@@ -1,12 +1,26 @@
 import { auth } from "~/server/auth";
 import { redirect } from "next/navigation";
 import { BasePageShell } from "~/components/layout/BasePage/BasePageShell";
+import { type Metadata } from "next";
+import { createTRPCContext } from "~/server/api/trpc";
+import { createCaller } from "~/server/api/root";
 
 interface BasePageProps {
   params: Promise<{ baseId: string }>;
 }
 
-// Params is passed by the app router with the value [baseId]
+export async function generateMetadata({ params }: BasePageProps): Promise<Metadata> {
+  const ctx = await createTRPCContext({ headers: new Headers() });
+  const caller = createCaller(ctx);
+
+  const { baseId } = await params;
+  const base = await caller.base.getBaseById({ baseId });
+
+  return {
+    title: base?.name ? `${base.name} - Not Airtable` : "Untitled Base - Not Airtable",
+  };
+}
+
 export default async function BasePage({ params }: BasePageProps) {
   const session = await auth();
 
